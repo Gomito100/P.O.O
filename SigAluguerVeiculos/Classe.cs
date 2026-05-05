5ee2a37cd255bec7431ef6a00184e25a7b308e83
Gomito100 <anselmo200325@gmail.com>
2026-05-05 13:59:10 +0100

    Add files via upload

---
+++ SigAluguerVeiculos/Classe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SigAluguerVeiculos
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string BI { get; set; }
+
+        public Cliente(string nome, string bi)
+        {
+            Nome = nome;
+            BI = bi;
+        }
+    }
+}
